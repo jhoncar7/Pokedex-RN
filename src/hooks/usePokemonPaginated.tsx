5160789bb7803699d968dc75880276f791c6b383
JhonCar <jhon7car@gmail.com>
2022-05-25 14:57:39 -0300

    avances

--- src/hooks/usePokemonPaginated.tsx
+++ src/hooks/usePokemonPaginated.tsx
@@ -1,27 +1,46 @@
 import { useEffect, useRef, useState } from "react";
 import { pokemonApi } from "../api/pokemonApi";
-import { PokemonPaginateResponse, Result, SimplePokemon } from "../interfaces/pokemonInterfaces";
+import { PokemonPaginateResponse, Result, SimplePokemon } from '../interfaces/pokemonInterfaces';
 
 
 
 export const usePokemonPaginated = () => {
 
+    const [isLoading, setIsLoading] = useState(true);
     const [simplePokemonList, setSimplePokemonList] = useState<SimplePokemon[]>([])
-
     const nextPageUrl = useRef('https://pokeapi.co/api/v2/ability/?limit=40');
 
     const loadPokemons = async () => {
+        setIsLoading(true);
         const resp = await pokemonApi.get<PokemonPaginateResponse>(nextPageUrl.current);
         nextPageUrl.current = resp.data.next;
         mapPokemonsList(resp.data.results);
     }
 
     const mapPokemonsList = (pokemonList: Result[]) => {
-        pokemonList.forEach(poke => console.log(poke.url))
+
+        const newPokemonList: SimplePokemon[] = pokemonList.map(({ name, url }) => {
+
+            const urlParts = url.split('/');
+            const id = urlParts[urlParts.length - 2];
+            const picture = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
+
+            return { id, picture, name }
+
+        });
+
+        setSimplePokemonList([...simplePokemonList, ...newPokemonList]);
+        setIsLoading(false);
     }
 
     useEffect(() => {
         loadPokemons();
     }, []);
+
+    return {
+        isLoading,
+        simplePokemonList,
+        loadPokemons,
+    }
 
 }
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,29 +1,60 @@
 import React from 'react'
-import { Button, Image, Text, View } from 'react-native'
+import { ActivityIndicator, Button, FlatList, Image, Text, View } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { usePokemonPaginated } from '../hooks/usePokemonPaginated';
 import { styles } from '../theme/appTheme';
+import { FadeInImage } from '../components/FadeInImage';
 
 
 export const HomeScreen = () => {
 
     const { top } = useSafeAreaInsets();
 
-    usePokemonPaginated();
+    const { isLoading, simplePokemonList, loadPokemons } = usePokemonPaginated();
+
 
     return (
         <>
-            <Image
-                source={require('../assets/pokebola.png')}
-                style={styles.pokebolaBG}
-            />
+            {
+                simplePokemonList.length > 0 &&
+                <>
+                    <Image
+                        source={require('../assets/pokebola.png')}
+                        style={styles.pokebolaBG}
+                    />
 
+                    <FlatList
+                        showsVerticalScrollIndicator={false}
+                        data={simplePokemonList}
+                        keyExtractor={(pok) => pok.id}
+                        renderItem={({ item }) => (
+                            <FadeInImage
+                                uri={item.picture}
+                                style={{ width: 100, height: 100 }}
+                            />
+                        )}
 
-            <Text style={{
+                        /* infinite scroll */
+                        onEndReached={loadPokemons}
+                        /* parte de abajo cuando llegue para que se dispare la nueva carga */
+                        onEndReachedThreshold={0.4} /* representa los ultimos 40% de abajo de la pantalla */
+
+                        ListFooterComponent={(
+                            <ActivityIndicator
+                                style={{ height: 100 }}
+                                size={30} // o tambien={50}
+                                color='grey'
+                            />
+                        )}
+                    />
+
+                    {/* <Text style={{
                 ...styles.title,
                 ...styles.globalMargin,
                 top: top + 20,
-            }}>Pokedex</Text>
+            }}>Pokedex</Text> */}
+                </>
+            }
         </>
     )
 }
