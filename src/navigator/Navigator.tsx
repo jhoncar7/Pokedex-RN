3b40e9fd528faa0b8061b4e2fbf88298d0514550
JhonCar <jhon7car@gmail.com>
2022-05-24 23:16:18 -0300

    avances

--- src/navigator/Navigator.tsx
+++ src/navigator/Navigator.tsx
@@ -7,7 +7,12 @@ const Stack = createStackNavigator();
 
 export const Navigator = () => {
     return (
-        <Stack.Navigator>
+        <Stack.Navigator
+            screenOptions={{
+                headerShown: false,
+                cardStyle: { backgroundColor: 'white' }
+            }}
+        >
             <Stack.Screen name="Home" component={HomeScreen} />
             <Stack.Screen name="Pokemon" component={PokemonScreen} />
         </Stack.Navigator>
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,23 +1,27 @@
 import React from 'react'
-import { Button, Text, View } from 'react-native'
+import { Button, Image, Text, View } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import Icon from 'react-native-vector-icons/Ionicons';
+import { styles } from '../theme/appTheme';
 
 
-export const HomeScreen = ({ navigation }: any) => {
+export const HomeScreen = () => {
 
+    const { top } = useSafeAreaInsets();
 
     return (
-        <View>
-            <Text>Home Screen</Text>
-
-            <Icon name="rocket" size={30} color='#5856D6' />
-
-            <Icon name="arrow-redo-outline" size={30} color="black" />
-
-            <Button
-                title="Go to Pokemon"
-                onPress={() => console.log(navigation.navigate('Pokemon'))}
+        <>
+            <Image
+                source={require('../assets/pokebola.png')}
+                style={styles.pokebolaBG}
             />
-        </View>
+
+
+            <Text style={{
+                ...styles.title,
+                ...styles.globalMargin,
+                top: top + 20,
+            }}>Pokedex</Text>
+        </>
     )
 }
