7e07314711da065e726684182e420b966275987b
JhonCar <jhon7car@gmail.com>
2022-05-24 21:51:04 -0300

    firts commit

---
+++ src/navigator/Navigator.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack';
+import { HomeScreen } from '../screens/HomeScreen';
+import { PokemonScreen } from '../screens/PokemonScreen';
+
+const Stack = createStackNavigator();
+
+export const Navigator = () => {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Pokemon" component={PokemonScreen} />
+        </Stack.Navigator>
+    );
+}
+
+
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button, Text, View } from 'react-native'
+
+export const HomeScreen = () => {
+    return (
+        <View>
+            <Text>Home Screen</Text>
+
+            <Button 
+                title="Go to Pokemon"
+            />
+        </View>
+    )
+}
